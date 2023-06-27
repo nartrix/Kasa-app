78eeddccc96497e9db983e4dbcde7f0811997148
nartrix <dimitri.danielo@laposte.net>
2023-06-27 12:43:38 +0200

    build home page

---
+++ src/components/card/index.jsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="title-apartment">
+          Titre de la <br />
+          location
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,24 @@
+import Card from "../../components/card";
+
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="section-background">
+        <div className="background-img">
+          <div className="section-content">
+            <h1 className="title">
+              Chez vous,
+              <br />
+              partout et ailleurs
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="section-card">
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
