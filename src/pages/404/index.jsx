2fcc27177b13f19b7052aa16e8771096cd52943b
nartrix <dimitri.danielo@laposte.net>
2023-06-27 13:08:52 +0200

    build 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Error() {
-  return <h1>404</h1>;
+  return (
+    <div className="error-404">
+      <div className="error-content">
+        <h1 className="error-title">404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+      </div>
+      <Link className="error-link" to="/">
+        Retourner sur la page d’accueil
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,6 +14,7 @@ function Home() {
           </div>
         </div>
       </div>
+
       <div className="section-card">
         <Card />
       </div>
